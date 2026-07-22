35f4c1b2e5af353c37edb7f3f743121add46e619
nigeltn <nguyenne.200507@gmail.com>
2026-07-21 20:38:05 -0400

    test(parser): edge coverage for day-2 grammar

--- tests/test_parser.cpp
+++ tests/test_parser.cpp
@@ -70,6 +70,121 @@ TEST(Parser, ConcatBindsTighterThanAlt) {
   EXPECT_EQ(p->right->val, 'c');
 }
 
+TEST(Parser, StarWrapsChar) {
+  rex::NodePtr p = rex::parse("a*");
+  ASSERT_NE(p, nullptr);
+  EXPECT_EQ(p->kind, rex::Kind::Star);
+  ASSERT_NE(p->left, nullptr);
+  EXPECT_EQ(p->left->kind, rex::Kind::Char);
+  EXPECT_EQ(p->left->val, 'a');
+  EXPECT_EQ(p->right, nullptr);
+}
+
+TEST(Parser, StarBindsToLastAtomOnly) {
+  rex::NodePtr p = rex::parse("ab*");
+  ASSERT_NE(p, nullptr);
+  EXPECT_EQ(p->kind, rex::Kind::Concat);
+  ASSERT_NE(p->left, nullptr);
+  EXPECT_EQ(p->left->kind, rex::Kind::Char);
+  EXPECT_EQ(p->left->val, 'a');
+  ASSERT_NE(p->right, nullptr);
+  EXPECT_EQ(p->right->kind, rex::Kind::Star);
+  ASSERT_NE(p->right->left, nullptr);
+  EXPECT_EQ(p->right->left->val, 'b');
+}
+
+TEST(Parser, StarThenMoreInput) {
+  rex::NodePtr p = rex::parse("a*b");
+  ASSERT_NE(p, nullptr);
+  EXPECT_EQ(p->kind, rex::Kind::Concat);
+  ASSERT_NE(p->left, nullptr);
+  EXPECT_EQ(p->left->kind, rex::Kind::Star);
+  ASSERT_NE(p->right, nullptr);
+  EXPECT_EQ(p->right->val, 'b');
+}
+
+TEST(Parser, StarOverAltBranch) {
+  rex::NodePtr p = rex::parse("a|b*");
+  ASSERT_NE(p, nullptr);
+  EXPECT_EQ(p->kind, rex::Kind::Alt);
+  ASSERT_NE(p->right, nullptr);
+  EXPECT_EQ(p->right->kind, rex::Kind::Star);
+}
+
+TEST(Parser, FourCharsFoldLeftDeep) {
+  rex::NodePtr p = rex::parse("abcd");
+  ASSERT_NE(p, nullptr);
+  EXPECT_EQ(p->kind, rex::Kind::Concat);
+  ASSERT_NE(p->right, nullptr);
+  EXPECT_EQ(p->right->val, 'd');
+  ASSERT_NE(p->left, nullptr);
+  EXPECT_EQ(p->left->kind, rex::Kind::Concat);
+  ASSERT_NE(p->left->left, nullptr);
+  EXPECT_EQ(p->left->left->kind, rex::Kind::Concat);
+  ASSERT_NE(p->left->left->left, nullptr);
+  EXPECT_EQ(p->left->left->left->val, 'a');
+}
+
+TEST(Parser, AltOfTwoConcats) {
+  rex::NodePtr p = rex::parse("ab|cd");
+  ASSERT_NE(p, nullptr);
+  EXPECT_EQ(p->kind, rex::Kind::Alt);
+  ASSERT_NE(p->left, nullptr);
+  EXPECT_EQ(p->left->kind, rex::Kind::Concat);
+  ASSERT_NE(p->right, nullptr);
+  EXPECT_EQ(p->right->kind, rex::Kind::Concat);
+  ASSERT_NE(p->right->left, nullptr);
+  EXPECT_EQ(p->right->left->val, 'c');
+}
+
+TEST(Parser, StarOnLeftAltBranch) {
+  rex::NodePtr p = rex::parse("a*|b");
+  ASSERT_NE(p, nullptr);
+  EXPECT_EQ(p->kind, rex::Kind::Alt);
+  ASSERT_NE(p->left, nullptr);
+  EXPECT_EQ(p->left->kind, rex::Kind::Star);
+  ASSERT_NE(p->right, nullptr);
+  EXPECT_EQ(p->right->val, 'b');
+}
+
+TEST(Parser, TwoStarsSideBySide) {
+  rex::NodePtr p = rex::parse("a*b*");
+  ASSERT_NE(p, nullptr);
+  EXPECT_EQ(p->kind, rex::Kind::Concat);
+  ASSERT_NE(p->left, nullptr);
+  EXPECT_EQ(p->left->kind, rex::Kind::Star);
+  ASSERT_NE(p->right, nullptr);
+  EXPECT_EQ(p->right->kind, rex::Kind::Star);
+  ASSERT_NE(p->right->left, nullptr);
+  EXPECT_EQ(p->right->left->val, 'b');
+}
+
+TEST(Parser, SpaceIsLiteralChar) {
+  rex::NodePtr p = rex::parse(" ");
+  ASSERT_NE(p, nullptr);
+  EXPECT_EQ(p->kind, rex::Kind::Char);
+  EXPECT_EQ(p->val, ' ');
+}
+
+TEST(Parser, StarNodeHasNoRightChild) {
+  rex::NodePtr p = rex::parse("a*|b");
+  ASSERT_NE(p, nullptr);
+  ASSERT_NE(p->left, nullptr);
+  EXPECT_EQ(p->left->right, nullptr);
+}
+
+TEST(Parser, DoubleStarFails) { EXPECT_EQ(rex::parse("a**"), nullptr); }
+
+TEST(Parser, LeadingStarFails) { EXPECT_EQ(rex::parse("*a"), nullptr); }
+
+TEST(Parser, StarAfterPipeFails) { EXPECT_EQ(rex::parse("a|*b"), nullptr); }
+
+TEST(Parser, LoneOpenParenFails) { EXPECT_EQ(rex::parse("("), nullptr); }
+
+TEST(Parser, StrayCloseParenFails) { EXPECT_EQ(rex::parse("a)"), nullptr); }
+
+TEST(Parser, DoublePipeFails) { EXPECT_EQ(rex::parse("a||b"), nullptr); }
+
 TEST(Parser, TrailingPipeFails) { EXPECT_EQ(rex::parse("a|"), nullptr); }
 
 TEST(Parser, LeadingPipeFails) { EXPECT_EQ(rex::parse("|a"), nullptr); }
